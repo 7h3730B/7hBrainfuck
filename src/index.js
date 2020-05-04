562a422da4e9fd757b67f30164e9a72d0ca00e22
7h3730B <teoschulze5@gmail.com>
2020-05-04 13:16:50 +0200

    Added a Execute button

--- src/index.js
+++ src/index.js
@@ -13,38 +13,34 @@ class Interpreter {
     step() {
         _this = this;
         if (_this.programStep > _this.program.length) {
-            clearInterval(_this.interval);
-            playBtn.removeAttribute('hidden');
-            stepBtn.removeAttribute('hidden');
-            pauseBtn.setAttribute('hidden', true);
-            stopBtn.setAttribute('hidden', true);
+            stopbs();
             return;
         }
 
         if (!_this.tokens.includes(_this.program[_this.programStep])) {
-            _this.programStep += 1;
+            _this.programStep++;
             _this.step();
             return;
         }
 
         switch (_this.program[_this.programStep]) {
             case '>':
-                _this.memPos += 1;
+                _this.memPos++;
                 if (_this.mem.length <= _this.memPos) _this.mem.push(0);
                 break;
             case '<':
-                _this.memPos -= 1;
+                _this.memPos--;
                 if (_this.memPos < 0) {
                     // TODO: error Handling: Out of memory
                     console.log("AHHHHHHHHHHHHHHHH");
                 }
                 break;
             case '+':
-                _this.mem[_this.memPos] += 1;
+                _this.mem[_this.memPos]++;
                 if (_this.mem[_this.memPos] > 255) _this.mem[_this.memPos] = 0;
                 break;
             case '-':
-                _this.mem[_this.memPos] -= 1;
+                _this.mem[_this.memPos]--;
                 if (_this.mem[_this.memPos] <= -1) _this.mem[_this.memPos] = 255;
                 break;
             case '.':
@@ -79,13 +75,7 @@ class Interpreter {
                 }
                 break;
         }
-        _this.updateMemory();
         _this.programStep += 1;
-    }
-    updateMemory() {
-        for (let i = 0; i < this.mem.length; i++) {
-            console.log(i);
-        }
     }
 }
 
@@ -94,20 +84,19 @@ let playBtn = document.getElementById('play');
 let pauseBtn = document.getElementById('pause');
 let stopBtn = document.getElementById('stop');
 let continueBtn = document.getElementById('continue');
+let executeBtn = document.getElementById('execute');
 
-let delay = 50;
+let delay = 1;
 let interval;
 let interpreter = new Interpreter(document.getElementById('source').value, document.getElementById('output-pre'), document.getElementById("input"), document.getElementById("ptr-table"), interval);
 
 function play() {
-    interpreter.memPos = 0;
-    interpreter.mem = [0];
-    interpreter.programStep = 0;
     interpreter.output.innerHTML = '';
     interpreter = new Interpreter(document.getElementById('source').value, document.getElementById('output-pre'), document.getElementById("input"), document.getElementById("ptr-table"), interval);
     interval = setInterval(interpreter.step.bind(interpreter), delay);
     playBtn.setAttribute('hidden', true);
     stepBtn.setAttribute('hidden', true);
+    executeBtn.setAttribute('hidden', true);
     pauseBtn.removeAttribute('hidden');
     stopBtn.removeAttribute('hidden');
 }
@@ -117,7 +106,7 @@ function step() {
     interpreter.step();
 }
 
-function pause() {
+async function pause() {
     clearInterval(interval);
     pauseBtn.setAttribute('hidden', true);
     continueBtn.removeAttribute('hidden');
@@ -128,21 +117,35 @@ function continuef() {
     continueBtn.setAttribute('hidden', true);
     playBtn.setAttribute('hidden', true);
     stepBtn.setAttribute('hidden', true);
+    executeBtn.setAttribute('hidden', true);
     pauseBtn.removeAttribute('hidden');
     stopBtn.removeAttribute('hidden');
 }
 
 function stopbs() {
     clearInterval(interval);
-    interpreter.memPos = 0;
-    interpreter.mem = [0];
-    interpreter.programStep = 0;
-    interpreter.output.innerHTML = '';
     stopBtn.setAttribute('hidden', true);
     pauseBtn.setAttribute('hidden', true);
     continueBtn.setAttribute('hidden', true);
     playBtn.removeAttribute('hidden');
     stepBtn.removeAttribute('hidden');
+    executeBtn.removeAttribute('hidden');
+}
+
+function execute() {
+    playBtn.setAttribute('hidden', true);
+    stepBtn.setAttribute('hidden', true);
+    executeBtn.setAttribute('hidden', true);
+    pauseBtn.removeAttribute('hidden');
+    stopBtn.removeAttribute('hidden');
+    interpreter.output.innerHTML = '';
+    setTimeout(() => {}, 10);
+    interpreter = new Interpreter(document.getElementById('source').value, document.getElementById('output-pre'), document.getElementById("input"), document.getElementById("ptr-table"), interval);
+    while (interpreter.programStep <= interpreter.program.length) {
+        interpreter.step(interpreter);
+        setTimeout(() => {}, 2);
+    }
+    stopbs();
 }
 
 playBtn.onclick = play;
@@ -150,3 +153,4 @@ pauseBtn.onclick = pause;
 stopBtn.onclick = stopbs;
 continueBtn.onclick = continuef;
 stepBtn.onclick = step;
+executeBtn.onclick = execute;
